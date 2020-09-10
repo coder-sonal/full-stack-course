e188052374ea255e8e51da57555b0b4debd1feb8
Sonal <singh0809sonal@gmail.com>
2020-09-10 20:01:02 +0530

    Practice9

---
+++ Week-3/Day-3/Session-2/join_array.js
@@ -0,0 +1,9 @@
+var array = [ 'Sum', 'of', 'all', 'the', 'even' ];
+var i = 0;
+var string ="";
+while (i <= array.length-1){
+    var element = array[i];
+    string = string +" "+element;
+    i++;
+}
+console.log(string);
