4f3abfe6c9583a43dd18062b3630b15dcf026507
Sonal <singh0809sonal@gmail.com>
2020-09-11 14:00:52 +0530

    superSetArray

---
+++ Week-3/Day-4/Session-1/checkSupersetArrays.js
@@ -0,0 +1,28 @@
+var arrayFirst = [1,2,9,10,3,4,6];
+var arraySecond = [5,4,6];
+
+superSetArray(arrayFirst);
+
+function superSetArray(firstArray){
+    matchStatus = false;
+    matchCount = 0;
+    for (var i = 0; i <= firstArray.length-1; i++){
+        var char = firstArray[i];
+        for (var j = 0; j <= arraySecond.length-1; j++){
+             var char2 = arraySecond[j];
+             if (char == char2){
+             matchStatus = true;
+             matchCount = matchCount +1;
+            }
+             else{
+                matchStatus = false;
+            }
+        }
+         
+    }
+    if (matchCount == arraySecond.length){
+        console.log(arrayFirst, "SupetSet of" , arraySecond)
+    }
+    else{
+        console.log("Try another array")}
+}
