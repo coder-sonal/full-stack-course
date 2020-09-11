ba0d65afc6486cad3b7c975d2762deded524b7aa
Sonal <singh0809sonal@gmail.com>
2020-09-11 19:28:12 +0530

    test

---
+++ Week-3/Day-4/Session-2/account-1/collab_test/index.js
@@ -0,0 +1,28 @@
+var arrayFirst = [1,2,5,11,9];
+var arraySecond = [7,9,2,4];
+ 
+var arrayDifference = [];
+diffrenceArray(arrayFirst);
+
+function diffrenceArray(array){
+    var i = 0 ;
+    var arrayMatch = false;
+    for (i = 0; i <= array.length-1; i++){
+     var char = array[i];
+        for (var a = 0; a <= arraySecond.length-1; a++){
+            var char2 = arraySecond[a];
+            if (char == char2){
+                arrayMatch = true;
+                break;
+            }
+            else{
+                arrayMatch =false;
+            }
+        }
+    if (!arrayMatch){
+         arrayDifference.push(char);
+          }  
+    }
+    console.log(arrayFirst ,"difference", arraySecond ,"is",arrayDifference);
+}
+cd
