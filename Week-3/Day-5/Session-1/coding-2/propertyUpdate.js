fa79817526cea721e449de0b74cb70fb84a976fa
Sonal <singh0809sonal@gmail.com>
2020-09-12 19:33:10 +0530

    propertyUpdate.js

---
+++ Week-3/Day-5/Session-1/coding-2/propertyUpdate.js
@@ -0,0 +1,16 @@
+var userDitails = {
+    firstName: "Sonal",
+    lastName: "Singh",
+    email: "snsonali62@gmail.com",
+    password:  "abc@Sonal"
+}
+
+propertyUpdate(userDitails);
+
+function propertyUpdate(userData){
+    userData.firstName = "Arunabh";
+    userData.email = "singh0809@sonal@gmail.com";
+    userData.mobieNumber = 7881110006;
+}
+
+console.log(userDitails);
