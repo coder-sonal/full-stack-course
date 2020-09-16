1550842c5723901db92239cf3ae85e49e274c564
Sonal <singh0809sonal@gmail.com>
2020-09-17 00:29:40 +0530

    rotateMatrix.js

---
+++ Week-3/Day-5/Session-1/assignment/rotateMatrix.js
@@ -0,0 +1,23 @@
+var matrixFun =[
+                    [1,2,3],
+                    [4,5,6],
+                    [7,8,9]
+                ];
+var size = matrixFun.length-1;
+
+rotateMatrix(matrixFun);
+
+function rotateMatrix(clockwiseMatrix){
+    // console.log(clockwiseMatrix)
+    var rotatedMatrix = [[],
+                        [],
+                        []];
+    for (var i = 0; i <= size; i++){
+        for (var j = 0; j <= size; j++){
+            rotatedMatrix[j][size - i] = clockwiseMatrix[i][j];
+        }
+       
+    }
+    console.log(rotatedMatrix);
+}
+
