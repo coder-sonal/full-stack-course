3efbaf952772e07b534c2cfd9c9a365623108fdf
Sonal <singh0809sonal@gmail.com>
2020-09-21 22:14:55 +0530

    Grades

---
+++ Week-4/Day-1/Session-2/coding-2/gradeStudents.js
@@ -0,0 +1,27 @@
+var student = [{name : "Aman", marks : 70},
+               {name : "Aarav", marks : 50},
+               {name : "Ajay" , marks : 80},
+            {name : "Chetan", marks :30}];
+
+
+var finalResult = student.map(function(result){
+    var studentResult = {};
+    studentResult = result;
+    if (result.marks >= 90){
+        result.grade = 'A';
+    }
+    else if (result.marks < 90 && result.marks >= 80){
+        result.grade = 'B';
+    }
+    else if (result.marks < 80 && result.marks >= 60){
+        result.grade = 'C';
+    }
+    else if (result.marks < 60 && result.marks >= 40){
+        result.grade = 'D';
+    }
+    else if (result.marks < 40){
+        result.grade = 'F';
+    }
+    return (studentResult);
+})
+console.log(finalResult);
