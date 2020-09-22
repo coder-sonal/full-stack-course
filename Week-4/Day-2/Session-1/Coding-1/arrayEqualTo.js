842216d794832a6ecdf2ca123a13d5b4979f9abf
Sonal <singh0809sonal@gmail.com>
2020-09-22 18:21:10 +0530

    arrayEqualTo.js

--- Week-4/Day-2/Session-1/Coding-1/arrayEqualTo.js
+++ Week-4/Day-2/Session-1/Coding-1/arrayEqualTo.js
@@ -1,13 +1,13 @@
 var arr = [2,6,3,10,11,9,10];
 var tValue = 10;
-var res= [];
+var newArr= [];
 arr.forEach(function(a){
     if (a !== tValue){
         var square = 0;
         square = a*a;
-        res.push(square);
+        newArr.push(square);
     }
 },tValue);
 
-console.log(res);
+console.log(newArr);
 
