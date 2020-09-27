a07add1e8b1e762dd0363480a9c0120086bb6a37
Sonal <singh0809sonal@gmail.com>
2020-09-27 23:51:26 +0530

    Bonus Challenge2

--- Week-4/Day-5/coding-1/listeners.js
+++ Week-4/Day-5/coding-1/listeners.js
@@ -1,4 +1,5 @@
 var btn= document.getElementById("col");
+btn.addEventListener('click',switchColor);
 
 function random(number){
     return Math.floor (Math.random()*number);
@@ -8,5 +9,13 @@ function switchColor(){
     document.getElementById("col").style.color = randomColor;
 }
  
+var btn2 = document.getElementById("arrayColor");
+btn2.addEventListener('click', rdmColor);
+var i = 0;
 
-btn.addEventListener('click',switchColor);
+function rdmColor(){
+    var colorArray = ["red","green","blue","yellow","pink","orange"];
+        var color = colorArray[i];
+        document.getElementById("arrayColor").style.backgroundColor = color;
+        i++;
+}
