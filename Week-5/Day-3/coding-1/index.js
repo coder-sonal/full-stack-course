d6954c9691b6d5c6a151d88f87cc2d4ba4e45a22
Sonal <singh0809sonal@gmail.com>
2020-10-09 23:55:19 +0530

    Updated day3-coding1 index.js

--- Week-5/Day-3/coding-1/index.js
+++ Week-5/Day-3/coding-1/index.js
@@ -1,11 +1,22 @@
 
 function login(ev){
     ev.preventDefault();
+    debugger
     var name = document.getElementById("userName").value;
     var password = document.getElementById("exampleInputPassword1").value;
-    debugger;
-        localStorage.setItem("userID", name)
-        localStorage.setItem("loginTime" , new Date());
+    var names = [];
+    var times = [];
+
+    if(localStorage.getItem('userID') && localStorage.getItem('loginTime')) {
+        names = JSON.parse(localStorage.getItem('userID'));
+        times = JSON.parse(localStorage.getItem('loginTime'));
+    }
+    names.push(document.getElementById("userName").value);
+    times.push(new Date());
+
+    localStorage.setItem("userID", JSON.stringify(names))
+    localStorage.setItem("loginTime" , JSON.stringify(times));
+
     if (name === "Sonal" && password === "1"){
         window.location.pathname = "August2020/Week-5/Day-3/coding-1/dash.html";
     }else if(name === "Arunabh" && password === "2"){
