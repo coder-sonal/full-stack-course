5e6ed8994c653149aa416355b3e551a4d17d1d5a
Sonal <singh0809sonal@gmail.com>
2020-10-21 23:56:28 +0530

    JQuery Second coding Conetnt

---
+++ Week-6/Day-3/Session-1/content.js
@@ -0,0 +1,14 @@
+$("#first").click(function(){
+    $("#firstname").text("Sonal");
+});
+$("#last").click(function(){
+    $("#lastname").text("Singh");
+})
+$("#full").click(function(){
+    $("#fullname").text("Sonal Singh")
+})
+$("#reset").click(function(){
+    $("#firstname").text("Hello");
+    $("#lastname").text("Masai");
+    $("#fullname").text("School");
+})
