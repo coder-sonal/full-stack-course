f248389f775fdbb4b12123bc76add1aab82a2b70
Sonal <singh0809sonal@gmail.com>
2020-10-22 00:42:03 +0530

    JQuery 3rd coding Colour

---
+++ Week-6/Day-3/Session-1/colours.js
@@ -0,0 +1,25 @@
+$("#headOne").click(function(){
+    $("#head1").css("font-size","70px");
+})
+$("#headTwo").click(function(){
+    $("#head2").css({"font-size":"32px","padding":"10px"});
+})
+$("#headThree").click(function(){
+    $("#head3").css({"background-color":"black","color":"white"});
+})
+$("#headFour").click(function(){
+    $("#head4").css({"border":"2px solid black","margin-top":"10px","margin-bottom":"10px"});
+    $("#head5").css({"border":"2px solid black","margin-top":"10px","margin-bottom":"10px"});
+})
+$("#con").click(function(){
+    $("div").css("padding","6px 8px");
+});
+$("#para").click(function(){
+    $("p").css({"color":"purple","font-weight":"bold"});
+});
+$("#red").click(function(){
+    $(".red").css("color","red");
+})
+$("#green").click(function(){
+    $(".green").css({"background-color":"green","color":"white"});
+})
