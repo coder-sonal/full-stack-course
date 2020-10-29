f0a338eb0591a6b9d1e63aae7158d8733989d406
Sonal <singh0809sonal@gmail.com>
2020-10-30 00:08:47 +0530

    ES6 coding

---
+++ Week-7/Day-2/Sesssion-1/display.js
@@ -0,0 +1,4 @@
+var userDetails = localStorage.getItem("registrationdetails");
+var name = window.location.search;
+name = name.split('?')[1];
+console.log(name);
--- Week-7/Day-2/Sesssion-1/index.js
+++ Week-7/Day-2/Sesssion-1/index.js
@@ -1,5 +1,8 @@
-
-$("#register").click(function(){
+let users = {};
+let count = 0;
+$("#register").click(function(e){
+  $("#userDetails").empty();
+  e.preventDefault();
   let fName = $("#firstName").val();
   let lName = $("#lastName").val(); 
   let uName = $("#userName").val(); 
@@ -8,17 +11,58 @@ $("#register").click(function(){
   let uAddress = $("#userAddress").val(); 
   let uMobile = $("#userMobile").val();
   let userType = $("input:radio[name=exampleRadios]:checked").val()
-  let userData = createUser(fName, lName , uName , userPwd , uEmail , uAddress , uMobile ,userType )
+  var a = (createUser(uName , userPwd , uEmail , userType ,fName ,lName ,uAddress , uMobile));
+  users[fName] = a;
+  showUserData();
+  var resdetails = JSON.stringify(users);
+  localStorage.setItem("registrationdetails", resdetails);
 })
 
-function createUser(fName = "userFirstName", lName = "userFirstName" ,uName , userPwd , uEmail , uAddress = "ACB Colony", uMobile = "1234056789" ,userType ){
-   debugger
-    let userDetails = {firstname: fName,
-                    lastname: lName,
-                    username: uName,
-                    password: userPwd,
-                    email: uEmail,
-                    address: uAddress,
-                    mobile: uMobile,
-                    type: userType}
+function showUserData(){
+    Object.keys(users).forEach(function(name){
+      var user = document.createElement("option");
+      user.innerHTML = name;
+      document.getElementById("userDetails").appendChild(user); 
+    })
+}
+
+$("#userDetails").click(function(){
+  var search = show();
+  window.location.href = "http://127.0.0.1:5500/August2020/Week-7/Day-2/Sesssion-1/display.html?"+search;
+  debugger;
+})
+
+function show(){
+    var name = $("#userDetails").val();
+    var personData = users[name];
+    var search = personData.firstname;
+    return search;
+    // if(count > 0){
+    //   $("#showdetails").empty();
+    // }
+    // count++;  
+    // $("#showdetails").append(`<p> Hello ${personData.firstname} , welcome back </p>`);
+    // $("#showdetails").append(`<p> First Name : ${personData.firstname} </p>`);
+    // $("#showdetails").append(`<p> Last Name  : ${personData.lastname} </p>`);
+    // $("#showdetails").append(`<p> User Name  : ${personData.username} </p>`);
+    // $("#showdetails").append(`<p> Password   : [Confidential]  </p>`);
+    // $("#showdetails").append(`<p> Email      : ${personData.email}  </p>`);
+    // $("#showdetails").append(`<p> Address    : ${personData.address} </p>`);
+    // $("#showdetails").append(`<p> Mobile No. : ${personData.mobile} </p>`);
+    // $("#showdetails").append(`<p> User Type  : ${personData.type} </p>`);
+}
+
+
+function createUser(uName , userPwd , uEmail, userType, fName, lName, uAddress, uMobile){
+    let userDetails = {
+      firstname: fName,
+      lastname: lName,
+      username: uName,
+      password: userPwd,
+      email: uEmail,
+      address: uAddress,
+      mobile: uMobile,
+      type: userType
+    }
+  return userDetails;
 }
