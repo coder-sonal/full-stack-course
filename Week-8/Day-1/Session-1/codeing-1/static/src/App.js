445ed9d2aae466aa3709b533fa96ab6b12fa1ad0
Sonal <singh0809sonal@gmail.com>
2020-11-19 00:25:22 +0530

    ReactApp Different buttons

---
+++ Week-8/Day-1/Session-1/codeing-1/static/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Static from './components/static';
+
+function App() {
+  return (
+    <div className="App">
+        <Static />
+    </div>
+  );
+}
+
+export default App;
---
+++ Week-8/Day-1/Session-1/codeing-1/static/src/components/static.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './static.css'
+
+const Static = () => {
+    return(
+        <div>
+            <div>
+                <button class="join">JOIN US</button>
+                <button class="setting">SETTINGS</button>
+            </div>
+            <div>
+                <button class="login">LOGIN</button>
+                <button class="contact">CONTACTS</button>
+            </div>
+            <div>
+                <button class="search">SEARCH</button>
+                <button class="help">HELP</button>
+            </div>
+            <div>
+                <button class="home">HOME</button>
+                <button class="download">DOWNLOAD</button>
+            </div>
+        </div>
+    )
+}
+export default Static;
