4c3011768775c8c19fe85a5ba2e9b02ab8ce101a
Sonal <singh0809sonal@gmail.com>
2020-11-18 23:44:36 +0530

    ReactApp Operationg system

---
+++ Week-8/Day-1/Session-1/codeing-1/operating/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Os from './components/os'
+
+function App() {
+  return (
+    <div>
+      <Os />
+    </div>
+  );
+}
+
+export default App;
---
+++ Week-8/Day-1/Session-1/codeing-1/operating/src/components/os.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './os.css'
+
+const Os = () => {
+    return (
+        <div>
+            <div>
+                <h2 class="header">Mobile Operating System</h2>
+                <ul class="list">
+                    <li>Android</li>
+                    <li>Blackberry</li>
+                    <li>iPhone</li>
+                    <li>Windows Phone</li>
+                </ul>
+            </div>
+            <div>
+                <h2 class="header">Mobile Manufacturing</h2>
+                <ul class="list">
+                    <li>Samsung</li>
+                    <li>HTC</li>
+                    <li>Apple</li>
+                    <li>Micromax</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+export default Os;
