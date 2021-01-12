a996d62207042c1544ada434314c1c6b52328caf
Sonal <singh0809sonal@gmail.com>
2021-01-12 12:53:01 +0530

    coding-1

--- Week-8/Day-2/Session-1/coding-1/src/components/ParentsComp.js
+++ Week-8/Day-2/Session-1/coding-1/src/components/ParentsComp.js
@@ -11,7 +11,7 @@ class ParentsComp extends React.Component{
                 time : 42,
                 reviews : "512 reviews",
                 costForOne : 799,
-                rating : 4.3,
+                rating : 2.3,
                 foodCategory : "Continental , Pizza , Asian , Indian , Desserts",
                 url :"https://lh3.googleusercontent.com/wdUvmhmr-UZy0dZ6M4Bzvz0C8EwCYlJniT3eBxauBVpV-TVFEu_1kgfag0hCFimizdg2jLm1yuqz8rJBub4GaeS9XCQ=w800"},
             {
@@ -21,7 +21,7 @@ class ParentsComp extends React.Component{
                 time : 30,
                 reviews : "19.9k reviews",
                 costForOne : 200,
-                rating : 4.2,
+                rating : 1.2,
                 foodCategory : "Continental , Pizza , Asian , Indian , Desserts",
                 url : "https://thumbor.thedailymeal.com/O5BS3X-3J3JKcsTKYdYd996xqsI=/870x565/https://www.thedailymeal.com/sites/default/files/slideshows/1943277/2108053/0.jpg"
             },
@@ -32,7 +32,7 @@ class ParentsComp extends React.Component{
                 time : 35,
                 reviews : "200 reviews",
                 costForOne : 100,
-                rating : 4.0,
+                rating : 1.0,
                 foodCategory : "North Indian",
                 url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSR5z7DhdslcFIPemA3No4ZLntS-uOy4zwBIQ&usqp=CAU"
             },
@@ -54,20 +54,20 @@ class ParentsComp extends React.Component{
                 time : 40,
                 reviews : "5.2k reviews",
                 costForOne : 99,
-                rating : 3.9,
+                rating : 2.9,
                 foodCategory : "pizza",
                 url : "https://media.architecturaldigest.in/wp-content/uploads/2020/04/Mumbai-restaurant-COVID-19-2-1.jpg"
             },
             {
-                name : "URU Brewpark",
+                name : "Cafe Mitrah",
                 paymentMode : "Accepts online payment only",
-                votes : 766,
-                time : 45,
-                reviews : "512 reviews",
-                costForOne : 799,
-                rating : 4.3,
+                votes : 500,
+                time : 30,
+                reviews : "700 reviews",
+                costForOne : 200,
+                rating : 4.0,
                 url : "https://media-cdn.tripadvisor.com/media/photo-s/08/ca/34/e3/korykos-turkish-restaurant.jpg",
-                foodCategory : "Continental , Pizza , Asian , Indian , Desserts"
+                foodCategory : "Cafe , Pizza, Burger , Sandwich"
             },
             {
                 name : "Burger King",
@@ -98,7 +98,7 @@ class ParentsComp extends React.Component{
                 time : 52,
                 reviews : "1,772 reviews",
                 costForOne : 200,
-                rating : 4.3,
+                rating : 3.3,
                 foodCategory : "North Indian",
                 url : "https://www.businesstimes.com.sg/sites/default/files/styles/article_img/public/image/2019/08/23/BT_20190823_TIFFIN1_3871185.jpg?itok=AjXY_9jX"
             },
@@ -115,7 +115,7 @@ class ParentsComp extends React.Component{
             }
         ]
     return(
-        <RestaurantDetails  restroDeatils = {restroData} />
+        <RestaurantDetails restroDeatils = {restroData} />
     )
     }
 }
